c3ad88f7146692b4a53241496b26e36a64d89335
Diogo-Honorato <diogoaugustomh@gmail.com>
2023-11-01 00:17:27 -0300

    Programando a funçao para adicionar aresta

--- listaAdjacente.c
+++ listaAdjacente.c
@@ -1,59 +1,66 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #define SIZE 11
 
-typedef struct Celula{
+typedef struct Celula
+{
 
     char vertice;
     int peso;
     struct Celula *proximo;
 
-}Celula;
+} Celula;
 
-typedef struct Lista{
+typedef struct Lista
+{
 
     Celula *primeiro;
     Celula *ultimo;
 
-}Lista;
+} Lista;
 
-typedef struct Grafo{
-    
+typedef struct Grafo
+{
+
     Lista *array;
 
-}Grafo;
+} Grafo;
 
+Grafo *criarGrafo(Grafo *grafo, int tamanho)
+{
 
-Grafo *criarGrafo(Grafo *grafo, int tamanho){
+    grafo = (Grafo *)malloc(sizeof(Grafo));
 
-    grafo = (Grafo*)malloc(sizeof(Grafo));
-
-    if (grafo == NULL) {
+    if (grafo == NULL)
+    {
         perror("Falha na alocação de memória");
         exit(1);
     }
 
-    grafo->array = (Lista*)malloc(tamanho * sizeof(Lista));
+    grafo->array = (Lista *)malloc(tamanho * sizeof(Lista));
 
-    for(int i = 0; i < tamanho; i++){
+    for (int i = 0; i < tamanho; i++)
+    {
 
         grafo->array[i].primeiro = NULL;
-   
+
         grafo->array[i].ultimo = NULL;
     }
 
     return grafo;
 }
 
-Celula *alocarCelula(char verticeAdjacente,int peso){
+Celula *alocarCelula(char verticeAdjacente, int peso)
+{
 
-    Celula *novaCelula = (Celula*)malloc(sizeof(Celula));
+    Celula *novaCelula = (Celula *)malloc(sizeof(Celula));
 
     novaCelula->peso = peso;
     novaCelula->vertice = verticeAdjacente;
     novaCelula->proximo = NULL;
 
-    if (novaCelula == NULL) {
+    if (novaCelula == NULL)
+    {
         perror("Falha na alocação de memória");
         exit(1);
     }
@@ -61,15 +68,45 @@ Celula *alocarCelula(char verticeAdjacente,int peso){
     return novaCelula;
 }
 
-
-int gerarChave(char verticeOrigem, int tamanho){
+int gerarChave(char verticeOrigem, int tamanho)
+{
 
     int numeroInteiro = verticeOrigem - 'A';
 
     return numeroInteiro % tamanho;
 }
 
-void adicionarAresta(Grafo *grafo, char verticeOrigem, char verticeDestino, int pesoAresta,int tamanho){
+int buscarVertice(Grafo *grafo, char vertice){
     
+    // Fazer um if-else para verificar se existe ou nao o vertice, se existe retorne o indice se nao retorne 0.
 
+}
+
+void adicionarAresta(Grafo *grafo, char verticeOrigem, char verticeDestino, int pesoAresta, int tamanho)
+{
+
+    int condicaoChave;
+    int chave = gerarChave(verticeOrigem, tamanho);
+
+    Celula *novaCelula = alocarCelula(verticeDestino,pesoAresta);
+
+    // Verificar se o vertice de origem em questao ja possui uma lista.
+
+    condicaoChave = buscarVertice(grafo,verticeOrigem);
+    
+    if (condicaoChave)
+    {
+        grafo->array[condicaoChave].ultimo->proximo = novaCelula;
+        grafo->array[condicaoChave].ultimo = novaCelula;
+    }
+    else
+    {
+        while (grafo->array[chave].primeiro != NULL)
+        {
+            chave = gerarChave(verticeOrigem + 1, tamanho);
+        }
+
+        grafo->array[chave].primeiro = novaCelula;
+        grafo->array[chave].ultimo = novaCelula;
+    }
 }
