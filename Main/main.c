1e73897eaf7b88ca941a7a8944d01299e28aeff1
Diogo-Honorato <diogoaugustomh@gmail.com>
2023-11-03 10:39:20 -0300

    adicionando os modulos de leitura de arquivo e um grafo.txt teste

--- Main/main.c
+++ Main/main.c
@@ -2,17 +2,12 @@
 #include <stdlib.h>
 #include "../Modulos/GraphLib.h"
 
-int main(){
+int main()
+{
 
     Grafo *G = criarGrafo();
 
-    char matriz[8][3] = {{'A','B','8'},{'Z','A','2'},{'V','Z','3'},{'R','Y','9'},{'B','C','7'},{'T','I','1'},{'C','R','5'},{'D','E','4'}};
-
-
-    for(int i = 0; i < 8; i++){
-
-        G = adicionarAresta(G,matriz[i][0],matriz[i][1],matriz[i][2]);
-    }
+    G = LeitorArquivo(G, "../inputs/grafo_1.txt");
 
     imprimirGrafo(G);
 
